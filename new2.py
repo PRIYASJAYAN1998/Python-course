845f79efacbe3b67bb378e364ae6eee4001ff7a3
PRIYASJAYAN1998 <130470711+PRIYASJAYAN1998@users.noreply.github.com>
2023-04-12 18:27:20 +0530

    Add files via upload

---
+++ new2.py
@@ -0,0 +1,10 @@
+a=[]
+even=[]
+for i in range(1,101):
+    if i%2==0:
+        even.append(i)
+    a.append(i)
+
+print(a)
+
+print(even)
---
+++ next.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the number:"))
+i=1
+while i<=x:
+ if i==6:
+ i=i+1
+ continue
+      print(i)
+      i=i+1
+
+ 
